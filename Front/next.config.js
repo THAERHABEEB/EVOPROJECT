8e23ac50ab38e5c67da72da54b6b69313eb598d7
THAERHABEEB <thaerhabeeb@gmail.com>
2026-06-23 00:51:13 +0300

    feat: enable Next.js static export for Render Static Site deployment

--- Front/next.config.js
+++ Front/next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   images: {
     domains: ['scontent.fcai30-1.fna.fbcdn.net'],
     unoptimized: true,
   },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
